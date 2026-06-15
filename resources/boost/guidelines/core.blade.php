b6c56ec0f6e8aba925f4bce588df075f17515e7a
krekas <krekas@gmail.com>
2026-06-15 11:10:41 +0300

    Add `laravel/boost` to dev dependencies and update boost guidelines

--- resources/boost/guidelines/core.blade.php
+++ resources/boost/guidelines/core.blade.php
@@ -1,3 +1,6 @@
+@php
+/** @var \Laravel\Boost\Install\GuidelineAssist $assist */
+@endphp
 ## laraveldaily/filacheck
 
 - After you have created/modified any files in `app/Filament` folder, you must run `{{ $assist->binCommand('filacheck --fix') }}`, to ensure there is no deprecated Filament code. Reported not fixed issues MUST be fixed before continuing.
